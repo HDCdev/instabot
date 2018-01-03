e44b5ea1cc0801e091dd71c1541afa568ec9715b
klashxx <klashxx@gmail.com>
2018-01-03 23:07:50 +0100

    directory positioning

--- instabot.py
+++ instabot.py
@@ -17,11 +17,12 @@ Options:
 
 import logging
 import os
+import sys
+from pathlib import Path
 
 import yaml
-from instapy import InstaPy
-
 from docopt import docopt
+from instapy import InstaPy
 
 logger = logging.getLogger('instabot')
 
@@ -29,9 +30,37 @@ VERSION = '0.1'
 CONFIG = './config.yml'
 
 
+def cd_insta_path():
+    global bot_path
+    bot_path = str(Path(__file__).resolve().parent)
+
+    try:
+        instapy_path = '{0}/src/instapy'.format(os.environ['VIRTUAL_ENV'])
+    except KeyError as error:
+        logger.critical('VIRTUAL_ENV var not set: %s', error)
+        sys.exit(5)
+
+    logger.debug('instapy path: %s', instapy_path)
+
+    if bot_path == instapy_path:
+        logger.critical('bot must not be installed over instapy dir')
+        sys.exit(5)
+
+    try:
+        os.chdir(instapy_path)
+    except FileNotFoundError:
+        logger.critical('cant access %s path', instapy_path)
+        sys.exit(5)
+
+    logger.info('positioned in instapy')
+
+    return None
+
+
 def get_config(config_file):
     with open(config_file) as stream:
         return yaml.load(stream)
+
 
 def set_logger(log_level):
     level = logging.getLevelName(log_level.upper())
@@ -45,6 +74,7 @@ def set_logger(log_level):
 
     return None
 
+
 def get_session():
 
     session = InstaPy(
@@ -54,6 +84,7 @@ def get_session():
     session.login()
 
     return session
+
 
 def main(arguments):
     config = arguments['CNF'] if arguments['CNF'] is not None else CONFIG
@@ -67,6 +98,8 @@ def main(arguments):
         logger.critical('unable to read file: %s', config)
         return None
 
+    cd_insta_path()
+
     try:
         session = get_session()
     except KeyError as error:
@@ -77,6 +110,10 @@ def main(arguments):
         return None
 
     session.end()
+    os.chdir(bot_path)
+
+    return None
+
 
 if __name__ == '__main__':
     main(docopt(__doc__, version=VERSION))
